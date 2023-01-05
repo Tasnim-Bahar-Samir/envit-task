67c8a3dcbf8674eaf74e8c57c7ce971855e336f1
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-01-05 14:40:22 +0600

    connected firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1>HEllo</h1>
+      <h1 className='text-5xl font-bold text-red-600'>HEllo</h1>
+      <Register/>
     </div>
   );
 }
