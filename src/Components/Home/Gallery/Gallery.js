a1482b52ef9926e5eb7995c0247c3724662da043
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-01-05 22:42:33 +0600

    added a gellery section

---
+++ src/Components/Home/Gallery/Gallery.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+import img1 from "../../../images/gallery/img1.jpg";
+import img2 from "../../../images/gallery/img2.webp";
+import img3 from "../../../images/gallery/img3.jpg";
+import img4 from "../../../images/gallery/img4.jpeg";
+import img5 from "../../../images/gallery/img5.jpeg";
+import img6 from "../../../images/gallery/img6.jpg";
+const Gallery = () => {
+  const imgages = [img1, img2, img3, img4, img5, img6, img6, img6];
+  return (
+    <div className="my-24">
+        <h4 className="text-center text-orange-600 text-3xl">Our Gallery</h4>
+        <h2 className="text-center text-4xl mb-6">See some images from our recent works</h2>
+      <div className="px-20 " style={{ columns: "3", columnGap: "0px" }}>
+        {imgages.map((img, i) => (
+          <PhotoProvider key={i}>
+            <PhotoView src={img}>
+              <img src={img} className="w-full block p-2" alt="" />
+            </PhotoView>
+          </PhotoProvider>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Contact from './Contact/Contact'
 import Banner from './Banner/Banner'
+import Gallery from './Gallery/Gallery'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
+        <Gallery/>
         <Contact/>
     </div>
   )
